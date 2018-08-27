e437e005e4144852a0e8187ece3c673affc991f0
neomie peretz <neoperetzmie@hotmail.com>
2018-08-27 13:03:20 +0300

    add comments2

--- main.js
+++ main.js
@@ -16,12 +16,10 @@ const pushToArray = function (userText) {
 const renderScreen = function () {
     $(".posts").empty()
     for (let i = 0; i < posts.length; i++) {
-
-
         let $post = $("<div class='post' data-id='" + posts[i].id + "'><p>" + posts[i].text + '</p>' +
             '<input type="text" class="usernameInput username form-control" placeholder="Username..."  />' +
             '<textarea  type="text" class="commentInput comment form-control" placeholder="Comments Here..."  />' +
-            '<button type="button" class="postComment btn postButton">Post Comment</button><button type="button" class="'+
+            '<button type="button" class="postComment btn postButton">Post Comment</button><button type="button" class="' +
             'removePost btn postButton">Remove Post</button></div>')
 
         AddComments($post, posts[i].comments);
@@ -30,32 +28,55 @@ const renderScreen = function () {
 
 
 }
+
 const AddComments = function ($post, comments) {
     for (let a = 0; a < comments.length; a++) {
-        $post.append("<div class='commentAdded'><h4>Username: " + comments[a].username + "</h4><p>Comment: " + comments[a].comment + "</p></div>")
+        $post.append("<div class='commentAdded' data-id='" + comments[a].id + "' ><h4>Username: " + comments[a].username + "</h4><p>Comment: " +
+            comments[a].comment + "</p><button type='button' class='removeComment btn commentButton'>Remove Comment</button></div>")
+
+    }
+}
+
+function getPostById(postID) {
+    for (let i = 0; i < posts.length; i++) {
+        if (postID === posts[i].id) {
+    return i;
+        }}
+}
+
+function getCommentById(commentID) {
+    getPostById(postID)
+    for (let a = 0; a < posts[i].comments.length; a++) {
+        if (postID === posts[i].id) {
+            if (commentID === posts[i].comments[a].id) {
+                return a;
+            }
+        }
     }
 }
 
 
-
-$(".posts").on('click', '.removePost', function () {
-    for (let i = 0; i < posts.length; i++) {
-        if ($(this).closest(".post").data().id === posts[i].id) {
-            posts.splice(i, 1)
-        }
-    }
+$(".commentAdded").on('click', '.removeComment', function () {
+    let i = getPostById($(this).closest(".post").data().id)
+    let a = getCommentById($(this).closest(".commentAdded").data().id)
+    posts[i].comments.splice(a, 1)
     renderScreen()
 })
 
+$(".posts").on('click', '.removePost', function () {
+    let i = getPostById($(this).closest(".post").data().id)
+    posts.splice(i, 1)
+    renderScreen()
+})
+
+let commentId = 0
 
 $("body").on('click', '.postComment', function () {
     let user = $(this).closest(".post").find(".usernameInput").val();
     let commentInput = $(this).closest(".post").find(".commentInput").val();
-    for (let i = 0; i < posts.length; i++) {
-        if ($(this).closest(".post").data().id === posts[i].id) {
-            posts[i].comments.push({ username: user, comment: commentInput });
-        }
-    };
+    let i = getPostById($(this).closest(".post").data().id)
+    posts[i].comments.push({ username: user, comment: commentInput, id: commentId });
+    commentId++
 
     renderScreen();
 
